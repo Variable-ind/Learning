6d3a92661b7446c08f70e735a0d4319173bfcb36
Variable <pkfayezakhtar@gmail.com>
2023-01-29 17:47:24 +0500

    learned arrays

--- test.c
+++ test.c
@@ -2,7 +2,14 @@
 
 #include <stdio.h>
 
-int main(){
-    printf("aassdaw");
+
+int halveit(int value){
+    return value/2;
+}
+
+int main(int argc, const char *argv[]){
+    float test[4] = {1.5, 2.4, 3.2, 4.7};
+    printf("test \n");
+    printf("%f \n", test[3]);
     return (0);
 }
