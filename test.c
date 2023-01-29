51cdc49c597ba6330446a4a46c2bf1d4d4a9ad71
Variable <pkfayezakhtar@gmail.com>
2023-01-29 20:41:42 +0500

    using the sdl library

--- test.c
+++ test.c
@@ -1,21 +1,19 @@
-
-
 #include "SDL2/SDL.h"
+
 
 int main(int argc, const char *argv[]){
     
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_Init(SDL_INIT_VIDEO);
-    // SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_VIDEO);
 
-    /*window = SDL_CreateWindow(
+    window = SDL_CreateWindow(
         "Test",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
         640, 480,
         0
     );
-*/
     return (0);
 }
