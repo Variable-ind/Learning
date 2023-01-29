4ac082798101fb7bb0992a978e6a53de698b7cc8
Variable <pkfayezakhtar@gmail.com>
2023-01-29 21:19:00 +0500

    Made a window

--- test.c
+++ test.c
@@ -15,5 +15,25 @@ int main(int argc, const char *argv[]){
         640, 480,
         0
     );
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    
+    SDL_SetRenderDrawColor(renderer, 0, 225, 255, 255);
+    
+    //clear the screen to blue
+    SDL_RenderClear(renderer);
+
+    // Display another rectangle
+    SDL_SetRenderDrawColor(renderer, 225, 225, 255, 255);
+    SDL_Rect rect = {220, 140, 200, 200};
+    SDL_RenderFillRect(renderer, &rect);
+
+    // Present what's drawn so far
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(5000);
+
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+
     return (0);
 }
