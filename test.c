460895b322e11e5aef1605023c0b5b55c2e79a0d
Variable <pkfayezakhtar@gmail.com>
2023-01-29 18:20:30 +0500

    Learnet most of it

--- test.c
+++ test.c
@@ -8,10 +8,5 @@ float halveit(const float tst[]){
 }
 
 int main(int argc, const char *argv[]){
-    float test[4] = {1.5, 2.4, 3.2, 4.7};
-    printf("test \n");
-    printf("before %f \n", test[3]);
-    printf("%f \n", halveit(test));
-    printf("after %f \n", test[3]);
     return (0);
 }
