0de9c43d5d5b6b689f3d87134e7af856609ba282
Variable <pkfayezakhtar@gmail.com>
2023-01-30 15:14:32 +0500

    add an actions function

--- test.c
+++ test.c
@@ -1,6 +1,40 @@
 #include "SDL2/SDL.h"
 
 
+
+int process_events(SDL_Window *window){
+    int done = 0;
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event))
+        {
+        switch (event.type)
+        {
+            case SDL_WINDOWEVENT_CLOSE:{
+                if (window){
+                    SDL_DestroyWindow(window);
+                    window = NULL;
+                    done = 1;
+                }                }
+            case SDL_KEYDOWN:{
+                switch (event.key.keysym.sym)
+                {
+                    case SDLK_ESCAPE:{
+                        done = 1;
+                    }
+                    break;
+                }
+            }
+            case SDL_QUIT:{
+                done = 1;
+            }
+            break;
+        }
+    }
+    return done;
+}
+
+
 int main(int argc, const char *argv[]){
     
     SDL_Window *window;
@@ -16,41 +50,16 @@ int main(int argc, const char *argv[]){
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     
     int done = 0;
-    SDL_Event event;
-
+    
     while(!done){
-        while (SDL_PollEvent(&event))
-        {
-            switch (event.type)
-            {
-                case SDL_WINDOWEVENT_CLOSE:{
-                    if (window){
-                        SDL_DestroyWindow(window);
-                        window = NULL;
-                    }
-                }
-                case SDL_KEYDOWN:{
-                    switch (event.key.keysym.sym)
-                    {
-                        case SDLK_ESCAPE:{
-                            done = 1;
-                        }
-                        break;
-                    }
-                }
-                case SDL_QUIT:{
-                    done = 1;
-                }
-                break;
-            }
+        if (process_events(window) == 1){
+            done = 1;
         }
-
+        done = process_events(window);
         SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
         SDL_RenderClear(renderer);
         SDL_RenderPresent(renderer);
         SDL_Delay(100);
     }
-
-
     return (0);
 }
